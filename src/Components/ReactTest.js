05ee9c7e6cbd00e2cf43238816117971a6552541
Kelly Phillips <fooxiefloof@gmail.com>
2019-08-19 15:09:30 -0700

    react playground

---
+++ src/Components/ReactTest.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ReactTest() {
+  return (
+    <h1>Hello Friends</h1>
+  )
+}
+
+export default ReactTest
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+function myApp() {
+  return (<ul><li>HelloWorld</li></ul>)
+}
+
+//The first argument is JSX
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
